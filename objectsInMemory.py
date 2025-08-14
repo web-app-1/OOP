4a6bc0664595454b91e632658007bb602c28d5e8
Luis <lcdel_rio@outlook.com>
2025-08-14 11:38:33 -0500

    ok

--- objectsInMemory.py
+++ objectsInMemory.py
@@ -1,1 +1,13 @@
-print(isinstance(5, object))
+class Backpack:
+    def __init__(self):
+        self._items = []
+
+@property
+def items(self):
+    return self._items
+
+my_bag = Backpack()
+your_bag = Backpack()
+
+print(id(my_bag))
+print(id(your_bag))
